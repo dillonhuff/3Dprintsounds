be73ee935c8925dbe88a32f3e54aa83f3e4e2a6f
Dillon Huff <dillonhuff@gmail.com>
2017-08-14 13:44:11 -0700

    Experimenting with SVM based classification

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,5 +1,7 @@
 from sklearn import svm
 
+import zaklow_spectrogram
+
 X = [[0, 1], [1, 1]]
 
 y = [0, 1]
@@ -12,3 +14,5 @@ pred = clf.predict([[2., 2.], [0, 0]])
 
 for p in pred:
     print p
+
+# Proper numpy based training data with spectrogram
